1401829915ba6ff547a4fd6e3527bf186b2071de
cl3b34 <cl3b34@gmail.com>
2018-04-15 15:39:19 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "beer",
+
+    'summary': """
+        Odoo module to support beer specific reporting in Finland
+        Supports Valvira and Tulli reporting""",
+
+    'description': """
+         Provide support for beer reporting to Valvira and Tulli in Finland
+    """,
+
+    'author': "Head Of Cool",
+    'website': "http://www.coolhead.fi",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['product'],
+    'application': True,
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+       # 'views/views.xml',
+        'views/product_inherited.xml',
+       # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+   # 'demo': [
+   #     'demo/demo.xml',
+   # ],
+}
