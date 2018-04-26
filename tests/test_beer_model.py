313e6da881fbfc4cddb94acc69939172c5b2dbda
cl3b34 <cl3b34@gmail.com>
2018-04-27 02:45:30 +0300

    fix unit test

--- tests/test_beer_model.py
+++ tests/test_beer_model.py
@@ -6,7 +6,7 @@ class TestBeerModel(TransactionCase):
     def test_create(self):
         "Create basic fields needed for reporting on beer sales"
         BeerReport = self.env['beer.beer_report']
-        beer = BeerReport.create({'alcohol_pct': 5.5,'liters': 0.33})
-        self.assertEqual(BeerReport.alcohol_pct, 5.5)
-        self.assertEqual(BeerReport.liters, 0.33)
+        beer = BeerReport.create({'alcohol_pct': 3,'liters': 0.33})
+        self.assertEqual(beer.alcohol_pct, 3)
+        self.assertEqual(beer.liters, 0.33)
         
